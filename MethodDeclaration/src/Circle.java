46822b0f6548e8847d91337f748930322957c4dc
Enrique Vidó Villalvazo <enriqu_vido23@outlook.com>
2024-10-13 15:02:59 -0600

    objects

---
+++ MethodDeclaration/src/Circle.java
@@ -0,0 +1,35 @@
+public class Circle {
+    private int x;
+    private int y;
+
+    Circle(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void moveCircle(Circle circle, int deltaX, int deltaY) {
+        circle.setX(circle.getX() + deltaX);
+        circle.setY(circle.getY() + deltaY);
+
+        System.out.println("X : " + circle.getX());
+        System.out.println("Y : " + circle.getY());
+
+        circle = new Circle(0, 0);
+    }
+}
--- MethodDeclaration/src/DeclarationMethod.java
+++ MethodDeclaration/src/DeclarationMethod.java
@@ -1,25 +1,45 @@
+import java.awt.*;
+
 public class DeclarationMethod {
 
-    public double computePayment(double loanAmt, double rate, double futureValue, int numPeriods) {
-        double interest = rate / 100.0;
-        double partial1 = Math.pow ((1 + interest), -numPeriods);
-        double denominator = (1 - partial1) / interest;
-        double answer = (-loanAmt / denominator) - ((futureValue * partial1) / denominator);
-        return answer;
-    }
-
-    public void printNumbers(int... numbers){
-        for (int number : numbers) {
-            System.out.print(number + " ");
-        }
-    }
+//    public double computePayment(double loanAmt, double rate, double futureValue, int numPeriods) {
+//        double interest = rate / 100.0;
+//        double partial1 = Math.pow ((1 + interest), -numPeriods);
+//        double denominator = (1 - partial1) / interest;
+//        double answer = (-loanAmt / denominator) - ((futureValue * partial1) / denominator);
+//        return answer;
+//    }
+//
+//    public void passMethod(int p){
+//        this.x = p;
+//        System.out.println(x);
+//    }
+//
+//    public void printNumbers(int... numbers){
+//        for (int number : numbers) {
+//            System.out.print(number + " ");
+//        }
+//        System.out.println();
+//    }
 
     public static void main(String[] args) {
-        DeclarationMethod loan = new DeclarationMethod();
-        System.out.println(loan.computePayment(100000, 4, 150000, 24));
+        Point originOne = new Point(23, 94);
+        Dimension dimension = new Dimension(100, 200);
+        Rectangle rectOne = new Rectangle(originOne, dimension);
+        Rectangle rectTwo = new Rectangle(50, 100);
 
-        DeclarationMethod numbers = new DeclarationMethod();
-        numbers.printNumbers(1, 2, 3);
+        System.out.println("Width of rectOne: " + rectOne.width);
+        System.out.println("Height of rectOne: " + rectOne.height);
+        System.out.println("Area of rectOne: " + (rectOne.width * rectOne.height));
+
+        rectTwo.setLocation(originOne);
+
+        System.out.println("rectTwo X Position: " + rectTwo.x);
+        System.out.println("rectTwo Y Position: " + rectTwo.y);
+
+        rectTwo.setLocation(40, 72);
+        System.out.println("rectTwo X Position: " + rectTwo.x);
+        System.out.println("rectTwo Y Position: " + rectTwo.y);
 
     }
 }
