8b3e2018e7beade7d4b54f29aa16da00ea54e480
Enrique Vidó Villalvazo <enriqu_vido23@outlook.com>
2024-10-21 10:03:46 -0600

    poo

--- EjerciciosPracticos/src/Equals.java
+++ EjerciciosPracticos/src/Equals.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Equals {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+//        String word;
+//
+//        do {
+//            System.out.print("Enter a word: ");
+//            word = input.nextLine();
+//        } while (!word.equalsIgnoreCase("Exit"));
+//
+//        System.out.println("Goodbye");
+        System.out.print("Enter a number from 1-100: ");
+        int limit = input.nextInt();
+
+        if (limit < 100) {
+            for (int i = 1; i <= limit; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            System.out.println("Goodbye");
+        } else System.out.println("Invalid number.");
+    }
 }
